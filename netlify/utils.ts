045a991b4bd46a2926a07b85aec3680ada84deb8
ThanIv <ivthankh@gmail.com>
2023-12-11 12:08:47 +0700

    update api error message

--- netlify/utils.ts
+++ netlify/utils.ts
@@ -32,7 +32,8 @@ export const corsOrigin = (origin, callback) => {
 export const checkApiKey = (req, res, next) => {
   const apiKey = req.headers["x-api-key"];
   if (apiKey !== process.env.API_KEY) {
-    return res.status(400).send(ERROR_MESSAGE.API_KEY_INVALID,apiKey,process.env.API_KEY);
+    // return res.status(400).send(ERROR_MESSAGE.API_KEY_INVALID,apiKey,process.env.API_KEY);
+    return res.status(400).send(apiKey);
   }
   next();
 };
